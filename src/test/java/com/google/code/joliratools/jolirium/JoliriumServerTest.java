58977102f06731bb229a90d18b1857e1be4d8599
joachim.kainz <joachim.kainz@c86e12c4-a0bc-11de-afa9-e300858c10cc>
2009-10-15 06:15:32 +0000

    git-svn-id: http://jolira-tools.googlecode.com/svn/jolirium/trunk@1457 c86e12c4-a0bc-11de-afa9-e300858c10cc

--- src/test/java/com/google/code/joliratools/jolirium/JoliriumServerTest.java
+++ src/test/java/com/google/code/joliratools/jolirium/JoliriumServerTest.java
@@ -1,16 +1,27 @@
 package com.google.code.joliratools.jolirium;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.junit.Test;
 
 public class JoliriumServerTest {
     @Test
-    public void testCreate() {
+    public void testCreate() throws IOException {
         JoliriumServer.setNextPortNumber(14567);
 
         final JoliriumServer server = new JoliriumServer();
         final Jolirium jolirium = server.getJolirium();
 
-        jolirium.open("http://www.google.com");
+        jolirium.open("http://eztv.it/shows/327/eastwick/");
+
+        final File tmp = File.createTempFile("screenshot", ".png");
+        final String path = tmp.getAbsolutePath();
+
+        jolirium.captureScreenshot(path);
         jolirium.close();
+
+        System.out.print("Screenshot at ");
+        System.out.println(path);
     }
 }
